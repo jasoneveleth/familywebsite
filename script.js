5adbf2fae5d2bc0b83a1df5af804978726dede21
Jason Eveleth <jj11@eveleth.com>
2021-01-16 14:44:33 -0500

    changed width to be 500

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ document.addEventListener("DOMContentLoaded", () => {
         alert("We don't have this feature yet!");
     };
 });
+
+// document.addEventListener("DOMContentLoaded", mobile);
+
+// function mobile() {
+//     let user = navigator.userAgent;
+//     if (user.includes("iPhone") || user.includes("Android")) {
+//         document.getElementsByClassName("title")[0].style.maxWidth = "600px";
+//     }
+// }
